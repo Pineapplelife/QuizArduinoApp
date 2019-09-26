ec37c16fd44b11e5c46cd31809c41768e662d004
Mathieu Gueribout <mgueribout@gmail.com>
2019-09-26 14:27:28 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: '', component: HomeComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { QuizFormComponent } from './quiz/quiz-form/quiz-form.component';
+import { SingleQuizComponent } from './quiz/single-quiz/single-quiz.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuizComponent,
+    QuizFormComponent,
+    SingleQuizComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
